499000b0d1c7885a99d8f44c02b64730553e0d23
belkud <vlad-belkud@yandex.ru>
2024-10-09 17:06:58 +0300

    jhiu

---
+++ checkJS/main.js
@@ -0,0 +1,99 @@
+import './style.css'
+
+
+console.log (5+3)
+console.log('dfsd');
+
+console.log(typeof '');
+console.log(typeof null);
+
+let a = ''
+console.log(a==false);
+
+let b
+console.log(typeof b);
+
+let c = 5
+console.log(typeof c);
+
+console.log(typeof alert);
+console.log(typeof prompt);
+
+console.log(+false);
+console.log(false);
+
+console.log(+true);
+console.log(true);
+
+
+
+console.log('b'>'a');
+console.log('x'>'X');
+
+// таблица юникод
+
+
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(false));
+console.log(Boolean(NaN));
+
+
+
+console.log( typeof undefined);
+console.log( typeof null);
+console.log( typeof NaN);
+
+let e = 10 
+if (e>5) {
+  console.log('успешно');  
+}
+
+
+
+
+
+
+
+console.log(null==null);
+console.log(5*'df');
+
+console.log(typeof(5+'df'));
+
+
+
+function readNumber(){
+  
+}
+
+
+let acc=0
+let num = [
+  8845,
+56050,
+5,
+781,
+142993,
+1094,
+103444,
+2764,
+8573,
+116,
+1,
+1,
+52840,
+30337,
+274978,
+34312,
+3,
+]
+
+
+num.forEach(el=> 
+  acc+=el
+)
+
+console.log(acc/1000_000);
+console.log(acc.toFixed(1));
