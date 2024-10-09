8e777eb6f1c1a8be2bd87c85dd14df04def7b4b6
belkud <vlad-belkud@yandex.ru>
2024-10-09 20:05:05 +0300

    fd

--- checkJS/main.js
+++ checkJS/main.js
@@ -66,39 +66,39 @@ console.log(typeof(5+'df'));
  
 
 
-let acc=0
-let num = [
-  8845,
-56050,
-5,
-781,
-142993,
-1094,
-103444,
-2764,
-8573,
-116,
-1,
-1,
-52840,
-30337,
-274978,
-34312,
-3,
-]
+// let acc=0
+// let num = [
+//   8845,
+// 56050,
+// 5,
+// 781,
+// 142993,
+// 1094,
+// 103444,
+// 2764,
+// 8573,
+// 116,
+// 1,
+// 1,
+// 52840,
+// 30337,
+// 274978,
+// 34312,
+// 3,
+// ]
 
 
-num.forEach(el=> 
-  acc+=el
-)
+// num.forEach(el=> 
+//   acc+=el
+// )
 
-console.log(acc/1000_000);
-console.log(acc.toFixed(1));
-
-
+// console.log(acc/1000_000);
+// console.log(acc.toFixed(1));
 
 
-// 1. Создайте скрипт, который запрашивает ввод двух чисел 
+
+
+//! 1. Создайте скрипт, который запрашивает ввод двух чисел 
 // (используйте prompt) и после показывает их сумму.
 
 // let x = Number(prompt('введите число 1'))
@@ -107,7 +107,7 @@ console.log(acc.toFixed(1));
 
 
 
-// 2. Как правильно округлить 6.35? на 6.4
+//! 2. Как правильно округлить 6.35? на 6.4
 
 // let x = 6.35
 // console.log(Math.round(x*10)/10);
@@ -115,7 +115,7 @@ console.log(acc.toFixed(1));
 
 
 
-// 3. Создайте функцию readNumber, которая будет запрашивать ввод 
+//! 3. Создайте функцию readNumber, которая будет запрашивать ввод 
 // числового значения до тех пор, пока посетитель его не введёт.
 
 // Функция должна возвращать числовое значение.
@@ -124,16 +124,55 @@ console.log(acc.toFixed(1));
 // отправив пустую строку или нажав «Отмена». В этом случае 
 // функция должна вернуть null.
 
-function readNumber() {
-  let x = +prompt('введите число')
-  if(x==0){
-  console.log('Процесс ввода остановлен');
-  } else if (x!=String) {
-    console.log(x);
-  } else{
-    readNumber()
-  }
+// function readNumber() {
+//   let num;
 
+//   do {
+//     num = prompt("Введите число", 0);
+//   } while ( !isFinite(num) );
+
+//   if (num === null || num === '') return null;
+//   return +num;
+// }
+
+// alert(`Число: ${readNumber()}`);
+
+
+
+//! 4. Этот цикл – бесконечный. Он никогда не завершится, почему?
+
+// let i = 0;
+// while (i != 10) {
+//   i += 0.2;
+// }
+
+
+//! 5.Напишите функцию random(min, max), которая генерирует 
+// случайное число с плавающей точкой от min до max (но не 
+// включая max).
+
+
+function randomss(min, max) {
+  return min + Math.random() * (max - min)
 }
 
-readNumber()
+console.log(randomss(1,5));
+
+
+// Напишите функцию randomInteger(min, max), которая генерирует 
+// случайное целое (integer) число от min до max (включительно).
+function randomInteger(min, max) {
+  // случайное число от min до (max+1)
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+console.log( randomInteger(1, 3) );
+
+
+
+
+let str = 'evening'
+
+
+
